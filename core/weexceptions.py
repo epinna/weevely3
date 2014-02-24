80d93742dea5f694927b358f25ac4043c403d4a9
Emilio Pinna <emilio.pinn@gmail.com>
2014-02-24 22:24:56 +0100

    Minor changes

--- core/weexceptions.py
+++ core/weexceptions.py
@@ -10,10 +10,6 @@ class DevException(Exception):
     pass
 
 
-""" Error communicating with basic interpreter """
-class InterpreterException(Exception):
-    pass
-
 """ Error on module execution """
 class ModuleError(Exception):
     pass
