f3d33f9e3ece54fed2f600367b4e57fc36fce56b
Emilio Pinna <emilio.pinn@gmail.com>
2014-09-03 00:07:56 +0200

    Minor adjustments

--- testsuite/test_shell_php.py
+++ testsuite/test_shell_php.py
@@ -9,10 +9,11 @@ class ShellPHP(BaseTest):
         session = sessions.start_session_by_url(self.url, self.password, volatile = True)
         modules.load_modules(session)
 
-        self.run_cmdline = modules.loaded['shell_php'].run_cmdline
+        self.run_argv = modules.loaded['shell_php'].run_argv
 
 
     def test_simple_commands(self):
 
-        self.assertEqual(self.run_cmdline("echo(1);"),"1");
+        self.assertEqual(self.run_argv(["echo(1);"]),"1");
+        self.assertEqual(self.run_argv(["echo(1)"]),"");
         
