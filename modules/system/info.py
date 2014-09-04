28b91857037199bfa73e013a781fcdbd7a0b1a84
Emilio Pinna <emilio.pinn@gmail.com>
2014-09-04 21:21:42 +0200

    Module system_info now uses the new vector object

--- modules/system/info.py
+++ modules/system/info.py
@@ -34,30 +34,32 @@ class Info(Module):
 
         self._register_vectors(
             [
-                Vector(
-                    'document_root', 'shell_php', "@print($_SERVER['DOCUMENT_ROOT']);"), Vector(
-                    'whoami', 'shell_php', "$u=@posix_getpwuid(posix_geteuid()); if($u) { $u = $u['name']; } else { $u=getenv('username'); } print($u);"), Vector(
-                    'hostname', 'shell_php', "@print(gethostname());"), Vector(
-                        'cwd', 'shell_php', "@print(getcwd());"), Vector(
-                            'open_basedir', 'shell_php', "$v=@ini_get('open_basedir'); if($v) print($v);"), Vector(
-                                'safe_mode', 'shell_php', "(ini_get('safe_mode') && print(1)) || print(0);"), Vector(
-                                    'script', 'shell_php', "@print($_SERVER['SCRIPT_NAME']);"), Vector(
-                                        'uname', 'shell_php', "@print(php_uname());"), Vector(
-                                            'os', 'shell_php', "@print(PHP_OS);"), Vector(
-                                                'client_ip', 'shell_php', "@print($_SERVER['REMOTE_ADDR']);"), Vector(
-                                                    'max_execution_time', 'shell_php', '@print(ini_get("max_execution_time"));'), Vector(
-                                                        'php_self', 'shell_php', '@print($_SERVER["PHP_SELF"]);'), Vector(
-                                                            'dir_sep', 'shell_php', '@print(DIRECTORY_SEPARATOR);'), Vector(
-                                                                'php_version', 'shell_php', "$v=''; if(function_exists( 'phpversion' )) { $v=phpversion(); } elseif(defined('PHP_VERSION')) { $v=PHP_VERSION; } elseif(defined('PHP_VERSION_ID')) { $v=PHP_VERSION_ID; } print($v);"), ])
+            Vector("print(@$_SERVER['DOCUMENT_ROOT']);", 'document_root'),
+            Vector("$u=@posix_getpwuid(@posix_geteuid());if($u){$u=$u['name'];} else{$u=getenv('username');} print($u);", 'whoami'),
+            Vector("print(@gethostname());", 'hostname'),
+            Vector("@print(getcwd());", 'cwd'),
+            Vector("$v=@ini_get('open_basedir'); if($v) print($v);", 'open_basedir'),
+            Vector("(@ini_get('safe_mode') && print(1)) || print(0);", 'safe_mode'),
+            Vector("print(@$_SERVER['SCRIPT_NAME']);", 'script'),
+            Vector("print(@php_uname());", 'uname'),
+            Vector("print(PHP_OS);", 'os'),
+            Vector("print(@$_SERVER['REMOTE_ADDR']);", 'client_ip'),
+            Vector('print(@ini_get("max_execution_time"));', 'max_execution_time'),
+            Vector('print(@$_SERVER["PHP_SELF"]);', 'php_self'),
+            Vector('@print(DIRECTORY_SEPARATOR);', 'dir_sep'),
+            Vector("$v=''; if(function_exists( 'phpversion' )) { $v=phpversion(); } elseif(defined('PHP_VERSION')) { $v=PHP_VERSION; } elseif(defined('PHP_VERSION_ID')) { $v=PHP_VERSION_ID; } print($v);", 'php_version')
+            ]
+        )
 
     def run(self, args):
 
         results = {}
-
+        
         for vector in self.vectors:
+            
             if args['info'] in ('all', vector.name):
-                results[vector.name] = self.terminal.run_shell_php(
-                    [vector.format()])
+
+                results[vector.name] = vector.run()
 
                 # Store "static" results used by other modules
                 if vector.name in ('whoami', 'hostname', 'dir_sep'):
