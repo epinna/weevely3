92c834c895e02a190cdd2dade10d082dcf09275b
Emilio Pinna <emilio.pinn@gmail.com>
2014-09-08 14:49:34 +0200

    Add tests for module shell_sh

---
+++ testsuite/test_shell_sh.py
@@ -0,0 +1,34 @@
+from testsuite.base_test import BaseTest
+from core.vector import Vector
+from core import modules
+from core import sessions
+from core import messages
+import logging
+import os
+
+class SystemInfo(BaseTest):
+
+    def setUp(self):
+        self.session = sessions.start_session_by_url(self.url, self.password, volatile = True)
+        modules.load_modules(self.session)
+
+        self.run_argv = modules.loaded['shell_sh'].run_argv
+
+    def test_run_unless(self):
+
+        vector_safe_name = 'proc_open'
+
+        # Spoil all the module sessions but the safe one
+        for i in range(0, len(modules.loaded['shell_sh'].vectors)):
+            name = modules.loaded['shell_sh'].vectors[i].name
+            payload = modules.loaded['shell_sh'].vectors[i].payload
+
+            if name != vector_safe_name:
+                modules.loaded['shell_sh'].vectors[i] = Vector('\'"%s' % payload, name)
+
+        # Check correctness of execution
+        self.assertEqual(self.run_argv(["echo 1"]), "1");
+
+        # Check stored vector
+        self.assertEqual(self.session['shell_sh']['options']['vector'], vector_safe_name)
+
