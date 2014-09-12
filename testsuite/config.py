2eccae19967e850c1e65e16b98967c9fcf7a68ba
Emilio Pinna <emilio.pinn@gmail.com>
2014-09-12 20:46:10 +0200

    Add useful command template in the testsuite config file

--- testsuite/config.py
+++ testsuite/config.py
@@ -1,7 +1,6 @@
-import os
 
-script_folder = 'testsuite/environment/'
-script_folder_url = 'http://localhost:9090/testsuite/environment/'
+#script_folder = 'testsuite/environment/'
+#script_folder_url = 'http://localhost:9090/testsuite/environment/'
 
 script_folder = '/var/www/environment/'
 script_folder_url = 'http://localhost/environment/'
@@ -13,4 +12,5 @@ test_generated_test_all_agents = True
 cmd_env_move_s_s = 'mv %s %s'
 cmd_env_chmod_s_s = 'chmod %s %s'
 cmd_env_mkdir_s = 'mkdir %s'
+cmd_env_rmdir_s = 'rmdir %s'
 cmd_env_remove_s = 'rm -f %s'
