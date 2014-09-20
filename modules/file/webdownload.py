3daa6026d177f10f122f2c91ffcb7a425e95a748
Emilio Pinna <emilio.pinn@gmail.com>
2014-09-20 22:23:50 +0200

    Add almost working webdownload module

---
+++ modules/file/webdownload.py
@@ -0,0 +1,52 @@
+from core.vector import PhpCmd, ShellCmd
+from core.module import Module
+from core import messages
+import random
+import datetime
+
+
+class Webdownload(Module):
+
+    """Download URL to the filesystem"""
+
+    def init(self):
+
+        self._register_info(
+            {
+                'author': [
+                    'Emilio Pinna'
+                ],
+                'license': 'GPLv3'
+            }
+        )
+
+        self._register_arguments(
+            arguments=[
+                'url',
+                'rpath'
+            ],
+            options={
+                'vector' : 'file_put_contents'
+            },
+            vector_argument = 'vector')
+
+        self._register_vectors(
+            [
+            PhpCmd("""@file_put_contents("${args['rpath']}", file_get_contents("${args['url']}"));""",
+              name = "file_put_contents"
+            ),
+            ShellCmd("""wget ${args['url']} -O ${args['rpath']}""",
+              name = "wget"
+            ),
+            ShellCmd("""curl -o ${args['rpath']} ${args['url']}""",
+              name = "curl"
+            )
+            ]
+        )
+
+    def run(self, args):
+
+        return self.vectors.get_result(
+         name = args['vector'],
+         arguments = { 'args' : args }
+        )
