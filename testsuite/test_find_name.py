d07d67b554b9a20b88083b92781cdc6dfaa7502f
Emilio Pinna <emilio.pinn@gmail.com>
2014-09-20 19:10:44 +0200

    First draft of find_name test

---
+++ testsuite/test_find_name.py
@@ -0,0 +1,78 @@
+from testfixtures import log_capture
+from testsuite.base_test import BaseTest
+from testsuite import config
+from core.sessions import SessionURL
+from core import modules
+from core import utilities
+from core import messages
+import core.utilities
+import subprocess
+import os
+
+class FindName(BaseTest):
+
+    def _recursive_folders(self, recursion = 4):
+
+        folders_abs = [ config.script_folder ]
+
+        for folder in [ utilities.randstr() for f in range(0, recursion) ]:
+            folders_abs.append(os.path.join(*[ folders_abs[-1], folder ] ))
+
+        folders_rel = [ f.replace(config.script_folder, '') for f in folders_abs[1:] ]
+
+        return folders_abs[1:], folders_rel
+
+    def setUp(self):
+        self.session = SessionURL(
+                    self.url,
+                    self.password,
+                    volatile = True
+                    )
+
+        modules.load_modules(self.session)
+
+        # Create the folder tree
+        self.folders_abs, self.folders_rel =  self._recursive_folders()
+
+        for folder_abs in self.folders_abs:
+            subprocess.check_call(
+                config.cmd_env_mkdir_s % (folder_abs),
+                shell=True)
+
+        files_names = [ 'test1', 'TEST1', 'TEST2', 'TEST3' ]
+
+        # Add a file in every folder and save paths
+        self.files_abs = []
+        self.files_rel = []
+        for folder_abs in self.folders_abs:
+            self.files_abs.append(os.path.join(folder_abs, files_names.pop(0)))
+            self.files_rel.append(self.files_abs[-1].replace(config.script_folder, ''))
+            subprocess.check_call(
+                config.cmd_env_content_s_to_s % ('1', self.files_abs[-1]),
+                shell=True)
+
+        self.run_argv = modules.loaded['find_name'].run_argv
+
+    def tearDown(self):
+
+        for folder in reversed(self.folders_abs):
+
+            subprocess.check_call(
+                config.cmd_env_remove_s % (self.files_abs.pop()),
+                shell=True)
+
+            subprocess.check_call(
+                config.cmd_env_rmdir_s % (folder),
+                shell=True)
+
+
+    @log_capture()
+    def test_find_name(self, log_captured):
+
+        # find file[0] (case insensitive, recursive, equal) -> file[0], file[1]
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertEqual(self.run_argv([ filen ]), './%s\n./%s' % (self.files_rel[0], self.files_rel[1]))
+
+        # find file[0] (case sensitive, recursive, equal) -> file[0]
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertEqual(self.run_argv([ '--case=True', filen ]), './%s' % (self.files_rel[0]))
