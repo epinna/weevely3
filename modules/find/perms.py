6c25f2194e745e750587202ee095c9ec85e9e35b
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-03 14:14:12 +0200

    Fix find_perms recursion for PHP payload

--- modules/find/perms.py
+++ modules/find/perms.py
@@ -23,22 +23,22 @@ class Perms(Module):
         self.register_vectors(
             [
             PhpCmd(
-              payload = """swp('${rpath}','${rpath}','${ type if type == 'd' or type == 'f' else '' }','${ '%s%s%s' % (('w' if writable else ''), ('r' if readable else ''), ('x' if executable else '') ) }','${ '1' if quit else '' }','${ True if recursive else False}');
+              payload = """swp('${rpath}','${rpath}','${ type if type == 'd' or type == 'f' else '' }','${ '%s%s%s' % (('w' if writable else ''), ('r' if readable else ''), ('x' if executable else '') ) }','${ '1' if quit else '' }', ${not no_recursion});
 function ckprint($df,$t,$a) { if(cktp($df,$t)&&@ckattr($df,$a)) { print($df."\\n"); return True;}   }
 function ckattr($df, $a) { $w=strstr($a,"w");$r=strstr($a,"r");$x=strstr($a,"x"); return ($a=='')||(!$w||is_writable($df))&&(!$r||is_readable($df))&&(!$x||is_executable($df)); }
 function cktp($df, $t) { return ($t==''||($t=='f'&&@is_file($df))||($t=='d'&&@is_dir($df))); }
-function swp($fdir, $d, $t, $a, $q,$r){
+function swp($fdir, $d, $t, $a, $q, $r){
 if($d==$fdir && ckprint($d,$t,$a) && ($q!="")) return;
 $h=@opendir($d); while ($f = @readdir($h)) { if(substr($fdir,0,1)=='/') { $df='/'; } else { $df=''; }
 $df.=join('/', array(trim($d, '/'), trim($f, '/')));
 if(($f!='.')&&($f!='..')&&ckprint($df,$t,$a) && ($q!="")) return;
-if(($f!='.')&&($f!='..')&&cktp($df,'d')&&$r){@swp($fdir, $df, $t, $a, $q,$r);}
+if(($f!='.')&&($f!='..')&&cktp($df,'d')&&$r){@swp($fdir, $df, $t, $a, $q, $r);}
 } if($h) { closedir($h); } }""",
              name = 'php_recursive',
              postprocess = lambda x: x.split('\n')
             ),
             ShellCmd(
-              payload = """find ${rpath} ${ '-maxdepth 1' if not recursive else '' } ${ '-print -quit' if quit else '' } ${ '-writable' if writable else '' } ${ '-readable' if readable else '' } ${ '-executable' if executable else '' } ${ '-type %s' % (type) if type == 'd' or type == 'f' else '' } 2>/dev/null""",
+              payload = """find ${rpath} ${ '-maxdepth 1' if no_recursion else '' } ${ '-print -quit' if quit else '' } ${ '-writable' if writable else '' } ${ '-readable' if readable else '' } ${ '-executable' if executable else '' } ${ '-type %s' % (type) if type == 'd' or type == 'f' else '' } 2>/dev/null""",
               name = "sh_find",
               postprocess = lambda x: x.split('\n')
             )
@@ -51,11 +51,12 @@ if(($f!='.')&&($f!='..')&&cktp($df,'d')&&$r){@swp($fdir, $df, $t, $a, $q,$r);}
           '-writable' : { 'action' : 'store_true' },
           '-readable' : { 'action' : 'store_true' },
           '-executable' : { 'action' : 'store_true' },
-          '-recursive' : { 'action' : 'store_true', 'default' : True },
+          '-no-recursion' : { 'action' : 'store_true', 'default' : False },
           '-vector' : { 'choices' : self.vectors.get_names(), 'default' : 'php_recursive' },
         })
 
     def run(self, args):
+        print args
         return self.vectors.get_result(args['vector'], args)
 
     def print_result(self, result):
