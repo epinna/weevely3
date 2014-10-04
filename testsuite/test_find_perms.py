c648eb07457a922b42c5dbc1186082abfd2152ed
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-04 21:33:04 +0200

    Add first test for find_perms draft

---
+++ testsuite/test_find_perms.py
@@ -0,0 +1,105 @@
+from testfixtures import log_capture
+from testsuite.base_fs import BaseFilesystem
+from testsuite import config
+from core.sessions import SessionURL
+from core import modules
+from core import utilities
+from core import messages
+import core.utilities
+import subprocess
+import os
+
+class FindPerms(BaseFilesystem):
+
+    def setUp(self):
+        self.session = SessionURL(
+                    self.url,
+                    self.password,
+                    volatile = True
+                    )
+
+        modules.load_modules(self.session)
+
+        # Create the folder tree
+        self.folders_abs, self.folders_rel =  self.populate_folders()
+        self.files_abs, self.files_rel = self.populate_files(
+                                self.folders_abs,
+                                [ 'executable', 'writable', 'write-executable', 'readable' ]
+                            )
+
+        # Change mode of the first file to ---x--x--x 0111 execute
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0111', self.files_abs[0]),
+            shell=True)
+
+        # Change mode of the second file to --w--w--w- 0222 write
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0222', self.files_abs[1]),
+            shell=True)
+
+        # Change mode of the third file to --wx-wx-wx 0333 write & execute
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0333', self.files_abs[2]),
+            shell=True)
+
+        # Change mode of the forth file to -r--r--r-- 0444 read
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0444', self.files_abs[3]),
+            shell=True)
+
+        # Change mode of the first folder to -rwxrwxrwx 0777 read, write, & execute
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0777', self.folders_abs[1]),
+            shell=True)
+
+        self.run_argv = modules.loaded['find_perms'].run_argv
+
+    def tearDown(self):
+
+        for folder in reversed(self.folders_abs):
+
+            subprocess.check_call(
+                config.cmd_env_remove_s % (self.files_abs.pop()),
+                shell=True)
+
+            subprocess.check_call(
+                config.cmd_env_rmdir_s % (folder),
+                shell=True)
+
+    def test_find_perms_php(self):
+
+        # find first writable starting from folder[0]
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-writable', '-quit', self.folders_rel[0] ]), [ self.folders_rel[1] ])
+
+        # find all executable starting from folder[0]
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-executable', self.folders_rel[0] ]), self.folders_rel + [  self.files_rel[2], self.files_rel[0] ] )
+
+        # find all readable starting from folder[0]
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-readable', self.folders_rel[0] ]), self.folders_rel + [  self.files_rel[3] ] )
+
+        # find readable starting from folder[0] with no recursion
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-readable', '-no-recursion', self.folders_rel[0] ]), self.folders_rel[:2] )
+
+
+    def test_find_perms_sh(self):
+
+        # find first writable starting from folder[0]
+        # Apparently, find shell command returns [0] as first element
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-writable', '-vector', 'sh_find', '-quit', self.folders_rel[0] ]), [ self.folders_rel[0] ])
+
+        # find all executable starting from folder[0]
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-executable', '-vector', 'sh_find', self.folders_rel[0] ]), self.folders_rel + [  self.files_rel[2], self.files_rel[0] ] )
+
+        # find all readable starting from folder[0]
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-readable', '-vector', 'sh_find', self.folders_rel[0] ]), self.folders_rel + [  self.files_rel[3] ] )
+
+        # find readable starting from folder[0] with no recursion
+        filep, filen = os.path.split(self.files_rel[0])
+        self.assertItemsEqual(self.run_argv([ '-readable', '-vector', 'sh_find', '-no-recursion', self.folders_rel[0] ]), self.folders_rel[:2] )
