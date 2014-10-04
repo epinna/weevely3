2fe91158bef2045e5ca6af41afde33b25f153dfe
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-04 19:23:34 +0200

    Add base_fs to automatize tests that interact with remote filesystem

---
+++ testsuite/base_fs.py
@@ -0,0 +1,54 @@
+from core.channels.channel import Channel
+from testsuite.base_test import BaseTest
+from core import utilities
+from testsuite import config
+import subprocess
+import random
+import os
+
+class BaseFilesystem(BaseTest):
+
+    def populate_folders(self, deepness = 4):
+        """Generate a folder tree with random names.
+
+        Args:
+            deepness (int): How much is deep the folder tree
+
+        Returns:
+            A set of two strings, dir_abs_path and dir_rel_path
+        """
+
+        folders_abs = [ config.script_folder ]
+
+        for folder in [ utilities.randstr() for f in range(0, deepness) ]:
+
+            folders_abs.append(os.path.join(*[ folders_abs[-1], folder ] ))
+            subprocess.check_call(
+                config.cmd_env_mkdir_s % (folders_abs[-1]),
+                shell=True)
+
+        folders_rel = [ f.replace(config.script_folder, '') for f in folders_abs[1:] ]
+
+        return folders_abs[1:], folders_rel
+
+    def populate_files(self, dir_abs_paths):
+
+        """Populate a folder tree with files with random names.
+
+        Args:
+            dir_abs_path (list of str): List of folders to populate
+
+        Returns:
+            A set of file_abs_path, file_rel_path
+        """
+
+        files_abs = []
+        files_rel = []
+        for folder_abs in dir_abs_paths:
+            self.files_abs.append(os.path.join(folder_abs, utilities.randstr()))
+            self.files_rel.append(self.files_abs[-1].replace(config.script_folder, ''))
+            subprocess.check_call(
+                config.cmd_env_content_s_to_s % ('1', self.files_abs[-1]),
+                shell=True)
+
+        return files_abs, files_rel
