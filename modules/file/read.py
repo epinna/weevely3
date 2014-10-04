372768ee1782cc53a1f7da661095c329fe6b73f6
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-04 18:33:11 +0200

    Add file_read

---
+++ modules/file/read.py
@@ -0,0 +1,40 @@
+from core.vectors import PhpCmd, ShellCmd, ModuleCmd, Os
+from core.module import Module
+from core import modules
+import tempfile
+
+class Read(Module):
+
+    """Read remote file from the remote filesystem."""
+
+    def init(self):
+
+        self.register_info(
+            {
+                'author': [
+                    'Emilio Pinna'
+                ],
+                'license': 'GPLv3'
+            }
+        )
+
+        self.register_arguments([
+          { 'name' : 'rpath', 'help' : 'Remote file path' },
+          { 'name' : '-vector', 'choices' : modules.loaded['file_download'].vectors.get_names() }
+        ])
+
+    def run(self, args):
+
+        # Get a temporary file name
+        temp_file = tempfile.NamedTemporaryFile()
+        args['lpath'] = temp_file.name
+
+        result = ModuleCmd(
+                    'file_download',
+                    [ args.get('rpath'), '${lpath}' ],
+                    name = 'file_download'
+                ).run(args)
+
+        temp_file.close()
+
+        return result
