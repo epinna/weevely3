da2703c94fc2f97e0ca822ce99219725d396b7c8
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-05 19:52:36 +0200

    Enhance file_download tests

--- testsuite/test_file_download.py
+++ testsuite/test_file_download.py
@@ -43,9 +43,67 @@ class FileDOwnload(BaseTest):
             config.cmd_env_remove_s % ('%s %s' % (self.file_ok, self.file_ko)),
             shell=True)
 
-    def test_download(self):
+    def test_download_php(self):
         temp_file = tempfile.NamedTemporaryFile()
 
-        # Correct download
+        # Simple download
         self.assertEqual(self.run_argv(['ok.test', temp_file.name]), 'OK')
         self.assertEqual(open(temp_file.name,'r').read(), 'OK')
+        temp_file.truncate()
+
+        # Downoad binary. Skip check cause I don't know the remote content, and
+        # the md5 check is already done inside file_download.
+        self.assertTrue(self.run_argv(['/bin/ls', temp_file.name]))
+        self.assertTrue(open(temp_file.name,'r').read(), 'OK')
+        temp_file.truncate()
+
+        # Download of an unreadable file
+        self.assertEqual(self.run_argv(['ko.test', temp_file.name]), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        # Download of an remote unexistant file
+        self.assertEqual(self.run_argv(['bogus', temp_file.name]), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        # Download to a local unexistant folder
+        self.assertEqual(self.run_argv(['ok.test', '/tmp/bogus/bogus']), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        # Download to a directory
+        self.assertEqual(self.run_argv(['ok.test', '/tmp/']), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        temp_file.close()
+
+
+    def test_download_sh(self):
+        temp_file = tempfile.NamedTemporaryFile()
+
+        # Simple download
+        self.assertEqual(self.run_argv(['-vector', 'base64', 'ok.test', temp_file.name]), 'OK')
+        self.assertEqual(open(temp_file.name,'r').read(), 'OK')
+        temp_file.truncate()
+
+        # Downoad binary. Skip check cause I don't know the remote content, and
+        # the md5 check is already done inside file_download.
+        self.assertTrue(self.run_argv(['-vector', 'base64', '/bin/ls', temp_file.name]))
+        self.assertTrue(open(temp_file.name,'r').read(), 'OK')
+        temp_file.truncate()
+
+        # Download of an unreadable file
+        self.assertEqual(self.run_argv(['-vector', 'base64', 'ko.test', temp_file.name]), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        # Download of an remote unexistant file
+        self.assertEqual(self.run_argv(['-vector', 'base64', 'bogus', temp_file.name]), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        # Download to a local unexistant folder
+        self.assertEqual(self.run_argv(['-vector', 'base64', 'ok.test', '/tmp/bogus/bogus']), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        # Download to a directory
+        self.assertEqual(self.run_argv(['-vector', 'base64', 'ok.test', '/tmp/']), None)
+        self.assertEqual(open(temp_file.name,'r').read(), '')
+
+        temp_file.close()
