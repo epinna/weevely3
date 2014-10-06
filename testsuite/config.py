16f947fac351c0380ed5ed48d8d696fadeee6d14
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-06 20:40:09 +0200

    Add explicative comments in testsuite config file

--- testsuite/config.py
+++ testsuite/config.py
@@ -1,18 +1,45 @@
+# Weevely testsuite configuration
 
-#script_folder = 'testsuite/environment/'
-#script_folder_url = 'http://localhost:9090/testsuite/environment/'
-
+# To get started with the testsuite, create a dedicated folder
+# in your web enviroinment and set permissions in order to give
+# total write, read and execute permissions to the web user.
+#
+# Then set the `script_folder` variable to the dedicated folder path
+# and set the `script_folder_url` variable to the corresponding URL
+# exposed through the web service.
+#
+# Example:
+# $ sudo mkdir /var/www/environment
+# $ sudo chown www-data:www-data /var/www/environment
+#
 script_folder = '/var/www/environment/'
 script_folder_url = 'http://localhost/environment/'
+#
+# If your web server is properly exposing the `script_folder_url`,
+# executing the command `python -m unittest discover` from the
+# weevely root folder should give the following output.
+#
+# $ python -m unittest discover
+# ...garbage...
+# ----------------------------------------------------------------------
+# Ran 31 tests in 26.780s
+# OK (skipped=4)
+# $
 
-debug = False
-
-test_generated_test_all_agents = True
-
-
+#
+# The following cmd_env_* commands are used to setup the
+# test enviroinment. Feel free to modify with ssh or other commands
+# to setup remote enviroinments.
+#
 cmd_env_move_s_s = 'mv %s %s'
 cmd_env_chmod_s_s = 'chmod %s %s'
 cmd_env_mkdir_s = 'mkdir %s'
 cmd_env_rmdir_s = 'rmdir %s'
 cmd_env_remove_s = 'rm -f %s'
 cmd_env_content_s_to_s = 'echo -n "%s" > %s'
+
+# Set to True to enable debug prints
+debug = False
+
+# This option is used by test_channels to reduce the requests
+test_generated_test_all_agents = False
