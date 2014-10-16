5d93eb54fd5937575d581eeb67265923ab4d9762
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-16 18:04:04 +0200

    Rewrite of the file_check test to work in restricted environments too

--- testsuite/test_file_check.py
+++ testsuite/test_file_check.py
@@ -1,66 +1,104 @@
-from testsuite.base_test import BaseTest
+from testsuite.base_fs import BaseFilesystem
 from testfixtures import log_capture
-from testsuite.config import script_folder
+from testsuite import config
 from core.sessions import SessionURL
 from core import modules
 from core import messages
+import subprocess
 import datetime
 import logging
 import os
 
-class FileCheck(BaseTest):
+class FileCheck(BaseFilesystem):
 
     def setUp(self):
         session = SessionURL(self.url, self.password, volatile = True)
         modules.load_modules(session)
 
+        # Create the folder tree
+        self.folders_abs, self.folders_rel =  self.populate_folders()
+        self.files_abs, self.files_rel = self.populate_files(
+                                self.folders_abs,
+                                [ 'executable', 'writable', 'write-executable', 'readable' ]
+                            )
+
+        # Change mode of the first file to 777
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0777', self.files_abs[0]),
+            shell=True)
+
+        # Get the epoch timestamp of file[0]
+        self.files_times = [
+            int(subprocess.check_output(
+                config.cmd_env_stat_time_modif_epoch_s % (self.files_abs[0]),
+                shell=True))
+        ]
+
+        # Change mode of the forth file to -r--r--r-- 0444 read
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0444', self.files_abs[3]),
+            shell=True)
+
+        # Change mode of the forth folder to --wx-wx-wx 0333 write & execute
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('0333', self.folders_abs[3]),
+            shell=True)
+
         self.run_argv = modules.loaded['file_check'].run_argv
+
+    def tearDown(self):
+
+        # Reset recursively all the permissions to 0777
+        subprocess.check_call(
+            config.cmd_env_chmod_s_s % ('-R 0777', self.folders_abs[0]),
+            shell=True)
+
+        for folder in reversed(self.folders_abs):
+
+            subprocess.check_call(
+                config.cmd_env_remove_s % (self.files_abs.pop()),
+                shell=True)
+
+            subprocess.check_call(
+                config.cmd_env_rmdir_s % (folder),
+                shell=True)
 
     def test_check(self):
 
-        # Some check on a privileged file
-        self.assertTrue(self.run_argv(['/etc/passwd', 'exists']))
-        self.assertTrue(self.run_argv(['/etc/passwd', 'readable']))
-        self.assertTrue(self.run_argv(['/etc/passwd', 'file']))
-        self.assertFalse(self.run_argv(['/etc/passwd', 'executable']))
-        self.assertFalse(self.run_argv(['/etc/passwd', 'writable']))
+        # Some check on a file just readable
+        self.assertTrue(self.run_argv([ self.files_rel[3], 'exists']))
+        self.assertTrue(self.run_argv([ self.files_rel[3], 'readable']))
+        self.assertTrue(self.run_argv([ self.files_rel[3], 'file']))
+        self.assertFalse(self.run_argv([ self.files_rel[3], 'executable']))
+        self.assertFalse(self.run_argv([ self.files_rel[3], 'writable']))
+        self.assertFalse(self.run_argv([ self.files_rel[3], 'dir']))
 
         # Some check on an unexistant file
-        self.assertFalse(self.run_argv(['/etc/BOGUS', 'exists']))
-        self.assertFalse(self.run_argv(['/etc/BOGUS', 'readable']))
-        self.assertFalse(self.run_argv(['/etc/BOGUS', 'file']))
-        self.assertFalse(self.run_argv(['/etc/BOGUS', 'executable']))
-        self.assertFalse(self.run_argv(['/etc/BOGUS', 'writable']))
+        self.assertFalse(self.run_argv(['BOGUS', 'exists']))
+        self.assertFalse(self.run_argv(['BOGUS', 'readable']))
+        self.assertFalse(self.run_argv(['BOGUS', 'file']))
+        self.assertFalse(self.run_argv(['BOGUS', 'executable']))
+        self.assertFalse(self.run_argv(['BOGUS', 'writable']))
+        self.assertFalse(self.run_argv(['BOGUS', 'dir']))
 
-        # Some check on an accessible folder
-        self.assertTrue(self.run_argv(['/tmp', 'exists']))
-        self.assertTrue(self.run_argv(['/tmp', 'readable']))
-        self.assertFalse(self.run_argv(['/tmp', 'file']))
-        self.assertTrue(self.run_argv(['/tmp', 'executable']))
-        self.assertTrue(self.run_argv(['/tmp', 'writable']))
+        # Some check on a folder with jsut x & w
+        self.assertTrue(self.run_argv([ self.folders_rel[3], 'exists']))
+        self.assertFalse(self.run_argv([ self.folders_rel[3], 'readable']))
+        self.assertFalse(self.run_argv([ self.folders_rel[3], 'file']))
+        self.assertTrue(self.run_argv([ self.folders_rel[3], 'executable']))
+        self.assertTrue(self.run_argv([ self.folders_rel[3], 'writable']))
+        self.assertTrue(self.run_argv([ self.folders_rel[3], 'dir']))
 
-        # Some check, with also md5 and time check
-        # First check if the current folder is writable
-        self.assertTrue(
-            self.run_argv(['.', 'writable']),
-            'Error: please set the script folder \'%s\' writable by the httpd user and re-run the test' % script_folder
-        )
-        # Create a test file
-        self.assertTrue(self.run_argv(['/tmp', 'writable']))
-        modules.loaded['shell_php'].run_argv(['file_put_contents("ftest","1");'])
+        # Save the human readable remote file[0] timestamp
+        rdatetime = datetime.datetime.fromtimestamp(float(self.files_times[0])).strftime('%Y-%m-%d')
 
-        # Get the remote timestamp
-        rtime = int(modules.loaded['shell_php'].run_argv(['print(time());']))
-        rdatetime = datetime.datetime.fromtimestamp(float(rtime)).strftime('%Y-%m-%d')
-
-        self.assertTrue(self.run_argv(['ftest', 'exists']))
-        self.assertTrue(self.run_argv(['ftest', 'readable']))
-        self.assertTrue(self.run_argv(['ftest', 'file']))
-        self.assertFalse(self.run_argv(['ftest', 'executable']))
-        self.assertTrue(self.run_argv(['ftest', 'writable']))
-        self.assertEqual(self.run_argv(['ftest', 'size']), 1)
-        self.assertEqual(self.run_argv(['ftest', 'md5']), 'c4ca4238a0b923820dcc509a6f75849b')
-        self.assertAlmostEqual(self.run_argv(['ftest', 'time']), rtime, delta = 20)
-        self.assertEqual(self.run_argv(['ftest', 'datetime']).split(' ')[0], rdatetime)
-
-        modules.loaded['shell_php'].run_argv(['unlink("ftest");'])
+        self.assertTrue(self.run_argv([ self.files_rel[0], 'exists']))
+        self.assertTrue(self.run_argv([ self.files_rel[0], 'readable']))
+        self.assertTrue(self.run_argv([ self.files_rel[0], 'file']))
+        self.assertFalse(self.run_argv([ self.files_rel[0], 'dir']))
+        self.assertTrue(self.run_argv([ self.files_rel[0], 'executable']))
+        self.assertTrue(self.run_argv([ self.files_rel[0], 'writable']))
+        self.assertEqual(self.run_argv([ self.files_rel[0], 'size']), 1)
+        self.assertEqual(self.run_argv([ self.files_rel[0], 'md5']), 'c4ca4238a0b923820dcc509a6f75849b')
+        self.assertAlmostEqual(self.run_argv([ self.files_rel[0], 'time']), self.files_times[0], delta = 20)
+        self.assertEqual(self.run_argv([ self.files_rel[0], 'datetime']).split(' ')[0], rdatetime)
