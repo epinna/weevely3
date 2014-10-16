e036984952a6d8e12e75665ba114d0ffbde0544c
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-16 23:17:57 +0200

    Add file_enum module

---
+++ modules/file/enum.py
@@ -0,0 +1,74 @@
+from core.vectors import PhpCode, ShellCmd, ModuleExec, Os
+from core.module import Module
+from core import modules
+
+class Enum(Module):
+
+    """Check existance and permissions of a list of paths."""
+
+    def init(self):
+
+        self.register_info(
+            {
+                'author': [
+                    'Emilio Pinna'
+                ],
+                'license': 'GPLv3'
+            }
+        )
+
+        self.register_arguments([
+          { 'name' : 'paths', 'help' : 'One or more paths', 'nargs' : '+' },
+          { 'name' : '-lpath-list', 'help' : 'The local file containing the list of paths' },
+          { 'name' : '-print', 'help' : 'Print the paths not found too', 'action' : 'store_true', 'default' : False }
+        ])
+
+    def run(self, args):
+
+        paths = []
+
+        if args.get('lpath_list'):
+
+            try:
+                paths = open(args.get('lpath_list'), 'r').read().split('\n')
+            except Exception, e:
+                log.warning(
+                  messages.generic.error_loading_file_s_s % (lpath, str(e)))
+                return
+
+        paths += args.get('paths') if args.get('paths') else []
+
+        results = {}
+
+        for path in paths:
+
+            result = ModuleExec( "file_check", [ path, "perms" ]).run()
+            if result or args.get('print'):
+                results[path] = result
+
+        return results
+
+
+    def print_result(self, result):
+
+        if not result: return
+
+        result_verbose = {}
+
+        for path, perms in result.items():
+
+            if len(perms) == 1 and perms[0] == 'e':
+                result_verbose[path] = 'exists'
+            else:
+                verbose_string = ' '.join([
+                    'writable' if 'w' in perms else '',
+                    'readable' if 'r' in perms else '',
+                    'executable' if 'x' in perms else ''
+                ])
+
+                # Re split to delete multiple whitespaces
+                result_verbose[path] = ' '.join(
+                    verbose_string.strip().split()
+                )
+
+        return Module.print_result(self, result_verbose)
