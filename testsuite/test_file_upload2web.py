3cc3d88a3d942c781ba75d0ebbca59d2ea7fcf6e
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-23 19:33:54 +0200

    Add certain file_upload2web tests

--- testsuite/test_file_upload2web.py
+++ testsuite/test_file_upload2web.py
@@ -28,14 +28,14 @@ class UploadWeb(BaseFilesystem):
             config.cmd_env_chmod_s_s % ('0777', self.folders_abs[0]),
             shell=True)
 
-        # Change mode of the folders [1:3] to -r-xr-xr-x 0555 read & execute
-        for fold in self.folders_abs[1:2]:
+        # Change mode of the folders [1] and [3] to -r-xr-xr-x 0555 read & execute
+        for fold in (self.folders_abs[1], self.folders_abs[3]):
             subprocess.check_call(
                 config.cmd_env_chmod_s_s % ('0555', fold),
                 shell=True)
 
         subprocess.check_call(
-            config.cmd_env_chmod_s_s % ('0777', self.folders_abs[3]),
+            config.cmd_env_chmod_s_s % ('0777', self.folders_abs[2]),
             shell=True)
 
         self.filenames = []
@@ -49,11 +49,15 @@ class UploadWeb(BaseFilesystem):
                 config.cmd_env_remove_s % (os.path.join(config.script_folder, f)),
                 shell=True)
 
-        subprocess.check_call(
-            config.cmd_env_chmod_s_s % ('-R 0777', self.folders_abs[0]),
-            shell=True)
+        # This has to be done before and in order, since in case of
+        # nonwritable/writable folders the writable one can't be deleted.
+        for folder in self.folders_abs:
+            subprocess.check_call(
+                config.cmd_env_chmod_s_s % ('0777', folder),
+                shell=True)
 
         for folder in reversed(self.folders_abs):
+
             subprocess.check_call(
                 config.cmd_env_rmdir_s % (folder),
                 shell=True)
@@ -94,15 +98,35 @@ class UploadWeb(BaseFilesystem):
             )
         temp_file.close()
 
-
         # Upload lfile guessing first writable path from [1],
-        # that is [3]
+        # that is [2]
         temp_file = tempfile.NamedTemporaryFile()
         temp_folder, temp_filename = os.path.split(temp_file.name)
-        rpath, rurl = self._get_path_url(3, temp_filename)
+        rpath, rurl = self._get_path_url(2, temp_filename)
         self.filenames.append(rpath)
         self.assertEqual(
             self.run_argv([ temp_file.name, self.folders_rel[1] ]),
             [ ( rpath, rurl ) ]
             )
         temp_file.close()
+
+
+    @log_capture()
+    def test_uploadweb_errs(self, log_captured):
+
+        # Upload a not existant lpath
+        self.assertIsNone(self.run_argv([ 'bogus', self.folders_rel[0] ]))
+        self.assertEqual(log_captured.records[-1].msg[:18],
+                         messages.generic.error_loading_file_s_s[:18])
+
+        # Upload a not existant rpath
+        temp_file = tempfile.NamedTemporaryFile()
+        self.assertIsNone(self.run_argv([ temp_file.name, self.folders_rel[0] + '/bogus/bogus' ]))
+        self.assertEqual(log_captured.records[-1].msg,
+                         messages.module_file_upload.failed_upload_file)
+
+        # Upload a not writable folder
+        temp_file = tempfile.NamedTemporaryFile()
+        self.assertIsNone(self.run_argv([ temp_file.name, self.folders_rel[3] + '/bogus' ]))
+        self.assertEqual(log_captured.records[-1].msg,
+                         messages.module_file_upload.failed_upload_file)
