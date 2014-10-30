165a1d1f5b5c937145344fd4c92168297a4bdc9b
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-30 19:39:27 +0100

    Add working file_bzip2 module

---
+++ modules/file/bzip2.py
@@ -0,0 +1,52 @@
+from core.vectors import PhpFile, ModuleExec
+from core.module import Module
+from core import messages
+from core import modules
+from core.loggers import log
+import os
+
+class Bzip2(Module):
+
+    """Compress or expand bzip2 files."""
+
+    aliases = [ 'bzip2', 'bunzip2' ]
+
+    def init(self):
+
+        self.register_info(
+            {
+                'author': [
+                    'Emilio Pinna'
+                ],
+                'license': 'GPLv3'
+            }
+        )
+
+        self.register_vectors(
+            [
+            PhpFile(
+              payload_path = os.path.join(self.folder, 'php_bzip2.tpl'),
+              name = 'php_bzip2',
+            )
+            ]
+        )
+
+        self.register_arguments([
+          { 'name' : 'rpaths', 'help' : 'Remote file paths', 'nargs' : '+' },
+          { 'name' : '--decompress', 'action' : 'store_true', 'default' : False, 'help' : 'Simulate gunzip' },
+          { 'name' : '--keep', 'action' : 'store_true', 'default' : False, 'help' : 'Keep (don\'t delete) input files' },
+        ])
+
+    def run(self, args):
+
+        # The correct execution returns something only on errors
+        result_err = self.vectors.get_result(
+            name = 'php_bzip2',
+            format_args = args,
+        )
+
+        if result_err:
+            log.warn(result_err)
+            return
+
+        return True
