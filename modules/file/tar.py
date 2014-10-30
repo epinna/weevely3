60035ce316d4f4d2fa84d10c3f7cfa8e0f496d1f
Emilio Pinna <emilio.pinn@gmail.com>
2014-10-30 22:46:27 +0100

    Add support for tgz and tbz to file_tar module

--- modules/file/tar.py
+++ modules/file/tar.py
@@ -41,6 +41,13 @@ class Tar(Module):
 
     def run(self, args):
 
+        if args.get('z'):
+            ModuleExec('file_gzip', [ '--keep', '--decompress', args['rtar'] ]).run()
+            args['rtar'] = ''.join(args['rtar'].split('.')[:-1])
+        elif args.get('j'):
+            ModuleExec('file_bzip2', [ '--keep', '--decompress', args['rtar'] ]).run()
+            args['rtar'] = ''.join(args['rtar'].split('.')[:-1])
+
         # The correct execution returns something only on errors
         result_err = self.vectors.get_result(
             name = 'php_tar',
