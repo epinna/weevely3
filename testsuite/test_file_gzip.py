cf7888d73060971a032d982efec82c6123fd8cf5
Emilio Pinna <emilio.pinn@gmail.com>
2014-11-05 22:32:37 +0100

    Add tests for compressing and decompressing of multiple files for file_gzip and file_bzip2 modules

--- testsuite/test_file_gzip.py
+++ testsuite/test_file_gzip.py
@@ -13,13 +13,23 @@ class FileGzip(BaseTest):
         session = SessionURL(self.url, self.password, volatile = True)
         modules.load_modules(session)
 
-        # Create and gzip a binary file for the test
-        self.binary_string = '\\xe0\\xf5\\xfe\\xe2\\xbd\\x0c\\xbc\\x9b\\xa0\\x8f\\xed?\\xa1\\xe1'
-        self.uncompressed = [ os.path.join(config.script_folder, 'binfile') ]
-        self.compressed = [ os.path.join(config.script_folder, 'binfile.gz') ]
+        # Create and gzip binary files for the test
+        self.string = [
+            '\\xe0\\xf5\\xfe\\xe2\\xbd\\x0c\\xbc\\x9b\\xa0\\x8f\\xed?\\xa1\\xe1',
+            '\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x06\\x00\\x00\\x00'
+             ]
+        self.uncompressed = [
+            os.path.join(config.script_folder, 'binfile0'),
+            os.path.join(config.script_folder, 'binfile1')
+            ]
+        self.compressed = [
+            os.path.join(config.script_folder, 'binfile0.gz'),
+            os.path.join(config.script_folder, 'binfile1.gz')
+            ]
 
-        self.check_call(config.cmd_env_content_s_to_s % (self.binary_string, self.uncompressed[0]))
-        self.check_call(config.cmd_env_gzip_s % (self.uncompressed[0]))
+        for index in range(0, len(self.string)):
+            self.check_call(config.cmd_env_content_s_to_s % (self.string[index], self.uncompressed[index]))
+            self.check_call(config.cmd_env_gzip_s % (self.uncompressed[index]))
 
         self.run_argv = modules.loaded['file_gzip'].run_argv
 
@@ -35,7 +45,7 @@ class FileGzip(BaseTest):
         self.assertTrue(self.run_argv(["--decompress", self.compressed[0]]));
         self.assertEqual(
             self.check_output(config.cmd_env_print_repr_s % self.uncompressed[0]),
-            self.binary_string
+            self.string[0]
         )
 
         # Let's re-compress it, and decompress and check again
@@ -43,7 +53,7 @@ class FileGzip(BaseTest):
         self.assertTrue(self.run_argv(["--decompress", self.compressed[0]]));
         self.assertEqual(
             self.check_output(config.cmd_env_print_repr_s % self.uncompressed[0]),
-            self.binary_string
+            self.string[0]
         )
 
         # Recompress it keeping the original file
@@ -56,9 +66,27 @@ class FileGzip(BaseTest):
         self.assertTrue(self.run_argv(["--decompress", self.compressed[0]]));
         self.assertEqual(
             self.check_output(config.cmd_env_print_repr_s % self.uncompressed[0]),
-            self.binary_string
+            self.string[0]
         )
 
+    def test_compress_decompress_multiple(self):
+
+        for index in range(0, len(self.compressed)):
+
+            # Decompress and check test file
+            self.assertTrue(self.run_argv(["--decompress", self.compressed[index]]));
+            self.assertEqual(
+                self.check_output(config.cmd_env_print_repr_s % self.uncompressed[index]),
+                self.string[index]
+            )
+
+            # Let's re-compress it, and decompress and check again
+            self.assertTrue(self.run_argv([self.uncompressed[index]]))
+            self.assertTrue(self.run_argv(["--decompress", self.compressed[index]]));
+            self.assertEqual(
+                self.check_output(config.cmd_env_print_repr_s % self.uncompressed[index]),
+                self.string[index]
+            )
 
     @log_capture()
     def test_already_exists(self, log_captured):
@@ -67,7 +95,7 @@ class FileGzip(BaseTest):
         self.assertTrue(self.run_argv(["--decompress", self.compressed[0], '--keep']));
         self.assertEqual(
             self.check_output(config.cmd_env_print_repr_s % self.uncompressed[0]),
-            self.binary_string
+            self.string[0]
         )
 
         # Do it again and trigger that the file decompressed already exists
@@ -87,7 +115,7 @@ class FileGzip(BaseTest):
         self.assertTrue(self.run_argv(["--decompress", self.compressed[0]]));
         self.assertEqual(
             self.check_output(config.cmd_env_print_repr_s % self.uncompressed[0]),
-            self.binary_string
+            self.string[0]
         )
 
         # Decompress the decompressed, wrong ext
