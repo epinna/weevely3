853eda873d77eea2411f5edd80c4f8cb2b16ad6a
Emilio Pinna <emilio.pinn@gmail.com>
2014-11-23 17:18:15 +0000

    Add system_extensions module to collect PHP extension and Apache module list

---
+++ modules/system/extensions.py
@@ -0,0 +1,56 @@
+from core.vectors import PhpCode
+from core.module import Module
+from core import messages
+import random
+
+
+class Extensions(Module):
+
+    """Collect PHP and webserver extension list."""
+
+    def init(self):
+
+        self.register_info(
+            {
+                'author': [
+                    'Emilio Pinna'
+                ],
+                'license': 'GPLv3'
+            }
+        )
+
+        self.register_vectors(
+            [
+            PhpCode("""
+                $f='get_loaded_extensions';
+                if(function_exists($f)&&is_callable($f))
+                    foreach($f() as $o) print($o.PHP_EOL);
+            """, 'php_extensions'),
+            PhpCode("""
+                $f='apache_get_modules';
+                if(function_exists($f)&&is_callable($f))
+                    foreach($f() as $o) print($o.PHP_EOL);
+            """, 'apache_modules'),
+            ]
+        )
+
+        self.register_arguments([
+          { 'name' : '-info',
+            'help' : 'Select modules or extensions',
+            'choices' : self.vectors.get_names(),
+            'nargs' : '+' }
+        ])
+
+    def run(self, args):
+
+        result = self.vectors.get_results(
+            names = args.get('info', [])
+        )
+
+        # Returns a string when a single information is requested,
+        # else returns a dictionary containing all the results.
+        info = args.get('info')
+        if info and len(info) == 1:
+            return result[info[0]]
+        else:
+            return result
