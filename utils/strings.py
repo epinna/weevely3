e90e91940a070551e49f8af54150e15a97863d0a
Emilio Pinna <emilio.pinn@gmail.com>
2014-12-09 23:22:04 +0000

    Add chunks function in utils.strings - even if is not related to string - due to my laziness

--- utils/strings.py
+++ utils/strings.py
@@ -40,3 +40,9 @@ def pollute(data, charset, frequency=0.3):
             str_encoded += char
 
     return str_encoded
+
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
