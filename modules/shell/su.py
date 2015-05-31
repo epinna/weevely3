98fb8dc69cd7e28ea8f7185b55ad5124f188c8b6
Emilio Pinna <emilio.pinn@gmail.com>
2015-05-31 12:49:20 +0100

    Fix sh_expect vector on shell_su module

--- modules/shell/su.py
+++ modules/shell/su.py
@@ -24,7 +24,7 @@ class Su(Module):
 
         self.register_vectors(
             [
-            ShellCmd("""expect -c 'spawn su -c "${command}" "${user}"; expect -re "assword"; send "${ passwd }\r\n"; expect eof;''""", "sh_expect"),
+            ShellCmd("""expect -c 'spawn su -c "${command}" "${user}"; expect -re "assword"; send "${ passwd }\r\n"; expect eof;'""", "sh_expect"),
             ShellCmd("""python -c 'import pexpect as p,sys;c=p.spawn("su ${user} -c ${command}");c.expect(".*assword:");c.sendline("${ passwd }");i=c.expect([p.EOF,p.TIMEOUT]);sys.stdout.write(c.before[3:] if i!=p.TIMEOUT else "")'""", "pyexpect")
             ]
         )
