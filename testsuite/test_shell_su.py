b96574678d5c3a01781a3b641bce966821603f3e
Emilio Pinna <emilio.pinn@gmail.com>
2015-05-31 13:15:04 +0100

    Add shell_su test in testsuite

---
+++ testsuite/test_shell_su.py
@@ -0,0 +1,33 @@
+from testsuite.base_test import BaseTest
+from core.weexceptions import ArgparseError
+from core.vectors import PhpCode
+from core.vectors import Os
+from core import modules
+from core.sessions import SessionURL
+from core import messages
+from testsuite.config import su_user, su_passwd
+import core.config
+import unittest
+import logging
+import os
+
+@unittest.skipIf(
+    not su_user or not su_passwd,
+    "Skip su shell tests")
+class ShellSu(BaseTest):
+
+    def setUp(self):
+        self.session = SessionURL(self.url, self.password, volatile = True)
+        modules.load_modules(self.session)
+
+        self.vector_list = modules.loaded['shell_su'].vectors.get_names()
+
+        self.run_argv = modules.loaded['shell_su'].run_argv
+
+
+    def test_param_vector(self):
+
+        for vect in self.vector_list:
+
+            # Check correctness of execution
+            self.assertEqual(self.run_argv(["-vector", vect, "-u", su_user, su_passwd, "whoami"]).rstrip(), "root");
