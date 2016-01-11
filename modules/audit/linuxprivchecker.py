42128da8cfa37c30af0ca091acf2fc2ae5f26c85
olivier dubasque <ode@localhost.localdomain>
2016-01-11 09:49:10 +0100

    embed linuxprivcheck in project

--- modules/audit/linuxprivchecker.py
+++ modules/audit/linuxprivchecker.py
@@ -1,6 +1,8 @@
 from core.vectors import ShellCmd, Os
 from core.module import Module
 from core import modules
+import base64
+import sys, os
 
 class Linuxprivchecker(Module):
 
@@ -21,7 +23,14 @@ class Linuxprivchecker(Module):
 
     def run(self):
 
-        pwdresult = ShellCmd('cd /tmp;wget www.securitysift.com/download/linuxprivchecker.py; python ./linuxprivchecker.py;rm /tmp/linuxprivchecker.py;').run()
+        path_name = os.path.dirname(sys.argv[0])
+        full_path = os.path.abspath(path_name)
+        script_path = os.path.join(full_path, 'utils',
+                              'linuxprivchecker.py')
+
+        content = base64.b64encode(open(script_path, 'r').read())
+
+        pwdresult = ShellCmd('cd /tmp;echo "%s" | base64 -d > /tmp/linuxprivchecker.py; python ./linuxprivchecker.py;rm /tmp/linuxprivchecker.py;' % content).run()
 
         if not pwdresult: return
         return pwdresult.rstrip('\n')
