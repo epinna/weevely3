f8502d277d81bcb94c909d2e9b800a8913a49322
Emilio Pinna <emilio.pinn@gmail.com>
2016-05-06 12:10:49 +0100

    Add http utils for direct HTTP requests

--- utils/http.py
+++ utils/http.py
@@ -1,8 +1,10 @@
 from core.weexceptions import FatalException
 from core import messages
 from core import config
+import random
 import string
 import utils
+import urllib2
 import os
 
 agents_list_path = 'utils/_http/user-agents.txt'
@@ -41,3 +43,12 @@ def add_random_url_param(url):
         url += '&%s' % random_param
 
     return url
+
+def request(url, headers = []):
+    
+    if not next((x for x in headers if x[0] == 'User-Agent'), False):
+        headers = [ ('User-Agent', random.choice(load_all_agents())) ]
+
+    opener = urllib2.build_opener()
+    opener.addheaders = headers
+    return opener.open(url).read()
