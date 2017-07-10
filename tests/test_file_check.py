e5fedec184d083f66c2f7869cd53c214891ff4ec
Emilio Pinna <emilio.pinn@gmail.com>
2017-07-10 20:21:58 +0100

    Update test_file_check

--- tests/test_file_check.py
+++ tests/test_file_check.py
@@ -1,6 +1,6 @@
-from testsuite.base_fs import BaseFilesystem
+from tests.base_test import BaseTest
 from testfixtures import log_capture
-from testsuite import config
+from tests import config
 from core.sessions import SessionURL
 from core import modules
 from core import messages
@@ -9,59 +9,47 @@ import datetime
 import logging
 import os
 
-class FileCheck(BaseFilesystem):
+def setUpModule():
+    subprocess.check_output("""
+BASE_FOLDER="{config.base_folder}/test_file_check/"
+mkdir -p "$BASE_FOLDER/dir1/dir2/dir3/0333"
+chmod 0333 "$BASE_FOLDER/dir1/dir2/dir3/0333"
+echo -n 1 > "$BASE_FOLDER/dir1/0777"
+chmod 0777 "$BASE_FOLDER/dir1/0777"
+touch "$BASE_FOLDER/dir1/dir2/writable"
+touch "$BASE_FOLDER/dir1/dir2/dir3/write-executable"
+touch "$BASE_FOLDER/dir1/dir2/dir3/0333/0444"
+chmod 0444 "$BASE_FOLDER/dir1/dir2/dir3/0333/0444"
+""".format(
+config = config
+), shell=True)
+
+class FileCheck(BaseTest):
+
+    files_rel = [
+        'test_file_check/dir1/0777',
+        'test_file_check/dir1/dir2/writable',
+        'test_file_check/dir1/dir2/dir3/write-executable',
+        'test_file_check/dir1/dir2/dir3/0333/0444',
+    ]
+    
+    file_0_time = int(subprocess.check_output(
+        'stat -c %%Y "%s"' % (os.path.join(config.base_folder, 'test_file_check/dir1/0777')),
+        shell=True)
+    )
+    
+    folders_rel = [
+        'test_file_check/dir1/',
+        'test_file_check/dir1/dir2/',
+        'test_file_check/dir1/dir2/dir3/',
+        'test_file_check/dir1/dir2/dir3/0333/',
+    ]
 
     def setUp(self):
         session = SessionURL(self.url, self.password, volatile = True)
         modules.load_modules(session)
 
-        # Create the folder tree
-        self.folders_abs, self.folders_rel =  self.populate_folders()
-        self.files_abs, self.files_rel = self.populate_files(
-                                self.folders_abs,
-                                [ 'executable', 'writable', 'write-executable', 'readable' ]
-                            )
-
-        # Change mode of the first file to 777
-        self.check_call(
-            config.cmd_env_chmod_s_s % ('0777', self.files_abs[0]),
-            shell=True)
-
-        # Get the epoch timestamp of file[0]
-        self.files_times = [
-            int(subprocess.check_output(
-                config.cmd_env_stat_time_modif_epoch_s % (self.files_abs[0]),
-                shell=True))
-        ]
-
-        # Change mode of the forth file to -r--r--r-- 0444 read
-        self.check_call(
-            config.cmd_env_chmod_s_s % ('0444', self.files_abs[3]),
-            shell=True)
-
-        # Change mode of the forth folder to --wx-wx-wx 0333 write & execute
-        self.check_call(
-            config.cmd_env_chmod_s_s % ('0333', self.folders_abs[3]),
-            shell=True)
-
         self.run_argv = modules.loaded['file_check'].run_argv
-
-    def tearDown(self):
-
-        # Reset recursively all the permissions to 0777
-        self.check_call(
-            config.cmd_env_chmod_s_s % ('-R 0777', self.folders_abs[0]),
-            shell=True)
-
-        for folder in reversed(self.folders_abs):
-
-            self.check_call(
-                config.cmd_env_remove_s % (self.files_abs.pop()),
-                shell=True)
-
-            self.check_call(
-                config.cmd_env_rmdir_s % (folder),
-                shell=True)
 
     def test_check(self):
 
@@ -90,7 +78,7 @@ class FileCheck(BaseFilesystem):
         self.assertTrue(self.run_argv([ self.folders_rel[3], 'dir']))
 
         # Save the human readable remote file[0] timestamp
-        rdatetime = datetime.datetime.fromtimestamp(float(self.files_times[0])).strftime('%Y-%m-%d')
+        rdatetime = datetime.datetime.fromtimestamp(float(self.file_0_time)).strftime('%Y-%m-%d')
 
         self.assertTrue(self.run_argv([ self.files_rel[0], 'exists']))
         self.assertTrue(self.run_argv([ self.files_rel[0], 'readable']))
@@ -100,5 +88,5 @@ class FileCheck(BaseFilesystem):
         self.assertTrue(self.run_argv([ self.files_rel[0], 'writable']))
         self.assertEqual(self.run_argv([ self.files_rel[0], 'size']), 1)
         self.assertEqual(self.run_argv([ self.files_rel[0], 'md5']), 'c4ca4238a0b923820dcc509a6f75849b')
-        self.assertAlmostEqual(self.run_argv([ self.files_rel[0], 'time']), self.files_times[0], delta = 20)
+        self.assertAlmostEqual(self.run_argv([ self.files_rel[0], 'time']), self.file_0_time, delta = 20)
         self.assertEqual(self.run_argv([ self.files_rel[0], 'datetime']).split(' ')[0], rdatetime)
