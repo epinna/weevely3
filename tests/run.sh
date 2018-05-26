412d09ca531e484d0b5cb4b1d8072b4a6cacf5b5
Emilio Pinna <emilio.pinn@gmail.com>
2018-05-26 21:45:02 +0100

    Better test runner

--- tests/run.sh
+++ tests/run.sh
@@ -4,18 +4,13 @@ set -e
 # Load docker defaults and check conf
 docker info
 
-ENTRYPOINT=""
-if [ "$1" = "bash" ];then
-    ENTRYPOINT=" --entrypoint /bin/bash"
-fi
-
 # Change folder to the root folder
 PARENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
 cd $PARENTDIR
 
 docker build -f tests/docker/Dockerfile . -t weevely
 docker rm -f weevely-inst || echo ''
-docker run  --rm --name weevely-inst -v `pwd`:/app/ -it${ENTRYPOINT} -p 80:80 -d weevely 
+docker run  --rm --name weevely-inst -v `pwd`:/app/ -p 80:80 -d weevely 
 
 # Wait until the http server is serving
 until $(curl --output /dev/null --silent --head http://localhost/); do
@@ -24,11 +19,14 @@ done
 
 if [ -z "$1" ]
   then
-    TEST='*'
+    docker exec -it weevely-inst python -m unittest discover ./tests/ "test_*.py"
+elif [ "$1" = "bash" ]
+  then
+    docker exec -it weevely-inst /bin/bash
 else
-    TEST="$1"
+    docker exec -it weevely-inst python -m unittest discover ./tests/ "test_$1.py"
 fi
 
-docker exec -it weevely-inst python -m unittest discover ./tests/ "test_$TEST.py"
+
 
 docker rm -f weevely-inst 
