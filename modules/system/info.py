e8d26c680d30d50f80667253a52f66b307dd1130
Lucien A <lu.aubert84@gmail.com>
2020-09-03 18:55:22 +0200

    Add info to `:system_info` and format size format for `:file_check`
    New info `server_ip` has been added to `results_to_store`.
    An argument has been added to `:system_info`: `-extended`. It enables new info to be gathered (as it can be a bit slow to gather everything).
    As `:system_info` now has too many values for its `-info` argument to let argparse build it's metavar. Metavar is now empty and possible values are displayed in help text.

--- modules/system/info.py
+++ modules/system/info.py
@@ -3,7 +3,7 @@ from core.module import Module
 from core import messages
 from core.loggers import log
 from core import modules
-import random
+import utils
 
 
 class Info(Module):
@@ -15,6 +15,17 @@ class Info(Module):
         'hostname',
         'pwd',
         'uname'
+    ]
+
+    default_provider = 'http://ifconfig.me/'
+
+    extended_vectors = [
+        'server_soft',
+        'server_ip',
+        'ini_path',
+        'tmp_path',
+        'free_space',
+        'dir_sep'
     ]
 
     def init(self):
@@ -30,65 +41,91 @@ class Info(Module):
 
         self.register_vectors(
             [
-            PhpCode("print(@$_SERVER['DOCUMENT_ROOT']);", 'document_root'),
-            PhpCode("""
-                if(is_callable('posix_getpwuid')&&is_callable('posix_geteuid')) {
-                    $u=@posix_getpwuid(@posix_geteuid());
-                    if($u){
-                        $u=$u['name'];
-                    } else {
-                        $u=getenv('username');
+                PhpCode("print(@$_SERVER['DOCUMENT_ROOT']);", 'document_root'),
+                PhpCode("@print(getcwd());", 'pwd'),
+                PhpCode("print(dirname(__FILE__));", 'script_folder'),
+                PhpCode("print(@$_SERVER['SCRIPT_NAME']);", 'script'),
+                PhpCode("print(@$_SERVER['PHP_SELF']);", 'php_self'),
+                PhpCode("""
+                    if(is_callable('posix_getpwuid')&&is_callable('posix_geteuid')) {
+                        $u=@posix_getpwuid(@posix_geteuid());
+                        if($u){
+                            $u=$u['name'];
+                        } else {
+                            $u=getenv('username');
+                        }
+                        print($u);
                     }
-                    print($u);
-                }
-            """, 'whoami'),
-            PhpCode("print(@gethostname());", 'hostname'),
-            PhpCode("@print(getcwd());", 'pwd'),
-            PhpCode("$v=@ini_get('open_basedir'); if($v) print($v);", 'open_basedir'),
-            PhpCode("(@ini_get('safe_mode') && print(1)) || print(0);", 'safe_mode',
-             postprocess = lambda x: True if x=='1' else False
-            ),
-            PhpCode("print(@$_SERVER['SCRIPT_NAME']);", 'script'),
-            PhpCode("print(dirname(__FILE__));", 'script_folder'),
-            PhpCode("print(@php_uname());", 'uname'),
-            PhpCode("print(@php_uname('s'));", 'os'),
-            PhpCode("print(@$_SERVER['REMOTE_ADDR']);", 'client_ip'),
-            PhpCode('print(@ini_get("max_execution_time"));', 'max_execution_time',
-             postprocess = lambda x: int(x) if x and x.isdigit() else False
-            ),
-            PhpCode('print(@$_SERVER["PHP_SELF"]);', 'php_self'),
-            PhpCode('@print(DIRECTORY_SEPARATOR);', 'dir_sep'),
-            PhpCode("""
-                $v='';
-                if(function_exists('phpversion')) {
-                    $v=phpversion();
-                } elseif(defined('PHP_VERSION')) {
-                    $v=PHP_VERSION;
-                } elseif(defined('PHP_VERSION_ID')) {
-                    $v=PHP_VERSION_ID;
-                }
-                print($v);""", 'php_version')
+                """, 'whoami'),
+                PhpCode("print(@gethostname());", 'hostname'),
+                PhpCode("$v=@ini_get('open_basedir'); if($v) print($v);", 'open_basedir'),
+                PhpCode("print(@ini_get('disable_functions'));", 'disable_functions'),
+                PhpCode("print(@php_ini_loaded_file());", 'ini_path'),
+                PhpCode("print(@sys_get_temp_dir());", 'tmp_path'),
+                PhpCode("print(@disk_free_space(__DIR__));", 'free_space',
+                        postprocess=lambda x: utils.prettify.format_size(int(x))),
+                PhpCode("print(@ini_get('safe_mode') ? 1 : 0);", 'safe_mode',
+                        postprocess=lambda x: True if x == '1' else False),
+                PhpCode("print(@$_SERVER['SERVER_SOFTWARE']);", 'server_soft'),
+                PhpCode("print(@php_uname());", 'uname'),
+                PhpCode("print(@php_uname('s') . ' ' . @php_uname('m'));", 'os'),
+                PhpCode("print(@$_SERVER['REMOTE_ADDR']);", 'client_ip'),
+                PhpCode("print(@file_get_contents('${provider}'));", 'server_ip'),
+                PhpCode("print(@$_SERVER['SERVER_NAME']);", 'server_name'),
+                PhpCode("print(@ini_get('max_execution_time'));", 'max_execution_time',
+                        postprocess=lambda x: int(x) if x and x.isdigit() else False),
+                PhpCode("@print(DIRECTORY_SEPARATOR);", 'dir_sep'),
+                PhpCode("""
+                    $v='';
+                    if(function_exists('phpversion')) {
+                        $v=phpversion();
+                    } elseif(defined('PHP_VERSION')) {
+                        $v=PHP_VERSION;
+                    } elseif(defined('PHP_VERSION_ID')) {
+                        $v=PHP_VERSION_ID;
+                    }
+                    print($v);
+                """, 'php_version')
             ]
         )
 
         self.register_arguments([
-          { 'name' : '-info',
-            'help' : 'Select information',
-            'choices' : self.vectors.get_names(),
-            'nargs' : '+' }
+            {'name': '-info',
+             'help': 'Select information (possible values are: %s)' % (', '.join(self.vectors.get_names())),
+             'choices': self.vectors.get_names(),
+             'default': [],
+             'nargs': '+',
+             'metavar': 'arg'},
+            {'name': '-extended',
+             'help': 'Get more info. Slower. (extended info: %s)' % (', '.join(self.extended_vectors)),
+             'action': 'store_true',
+             'default': False},
+            {'name': '-provider',
+             'help': 'The URL to get server_ip from (default: %s)' % self.default_provider,
+             'metavar': 'http://...',
+             'default': self.default_provider}
         ])
 
     def run(self):
 
+        vectors = self.args.get('info')
+
+        if not vectors and not self.args.get('extended'):
+            vectors = [i for i in self.vectors.get_names() if i not in self.extended_vectors]
+
         result = self.vectors.get_results(
-            names = self.args.get('info', []),
-            results_to_store = (
-                            'whoami',
-                            'hostname',
-                            'dir_sep',
-                            'os',
-                            'script_folder'
-            )
+            names=vectors,
+            results_to_store=(
+                'whoami',
+                'hostname',
+                'dir_sep',
+                'os',
+                'script_folder',
+                'server_ip'
+            ),
+            format_args={
+                'provider': self.args.get('provider')
+            }
         )
 
         # Returns a string when a single information is requested,
@@ -98,7 +135,6 @@ class Info(Module):
             return result[info[0]]
         else:
             return result
-
 
     def run_alias(self, args, cmd):
 
