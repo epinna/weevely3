57eb3e022ac8c18cf840019e72552165ba8e8fa6
Lucien A <lu.aubert84@gmail.com>
2022-09-27 18:30:50 +0200

    add -less option to :file_read

--- modules/file/read.py
+++ modules/file/read.py
@@ -1,13 +1,15 @@
+import subprocess
+
 from core.vectors import PhpCode, ShellCmd, ModuleExec, Os
 from core.module import Module
 from core import modules
 import tempfile
 
-class Read(Module):
 
+class Read(Module):
     """Read remote file from the remote filesystem."""
 
-    aliases = [ 'cat' ]
+    aliases = ['cat']
 
     def init(self):
 
@@ -21,26 +23,32 @@ class Read(Module):
         )
 
         self.register_arguments([
-          { 'name' : 'rpath', 'help' : 'Remote file path' },
-          { 'name' : '-vector', 'choices' : ( 'file', 'fread', 'file_get_contents', 'base64' ) }
+            {'name': 'rpath', 'help': 'Remote file path'},
+            {'name': '-less', 'help': 'Read output in less', 'action': 'store_true', 'default': False},
+            {'name': '-vector', 'choices': ('file', 'fread', 'file_get_contents', 'base64')}
         ])
 
     def run(self):
-
         # Get a temporary file name
         temp_file = tempfile.NamedTemporaryFile()
         self.args['lpath'] = temp_file.name
 
-        arg_vector = [ '-vector', self.args.get('vector') ] if self.args.get('vector') else []
+        arg_vector = ['-vector', self.args.get('vector')] if self.args.get('vector') else []
 
         # Run file_download
         result = ModuleExec(
-                    'file_download',
-                    [ self.args.get('rpath'), '${lpath}' ] + arg_vector,
-                    name = 'file_download'
-                ).run(self.args)
+            'file_download',
+            [self.args.get('rpath'), '${lpath}'] + arg_vector,
+            name='file_download'
+        ).run(self.args)
+
+        if self.args['less']:
+            subprocess.check_call(['less', temp_file.name])
 
         # Delete temp file
         temp_file.close()
+
+        if self.args['less']:
+            return ''
 
         return result
